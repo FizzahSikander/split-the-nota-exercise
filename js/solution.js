ee59e63e63643e3f3a755098cad6ea32e924ec70
Fizzah Sikander <123269709+FizzahSikander@users.noreply.github.com>
2023-11-06 22:19:49 +0100

    final work

---
+++ js/solution.js
@@ -0,0 +1,33 @@
+const showSumElem = document.querySelector('#showSum');
+const inputFormElem = document.querySelector('#inputForm');
+const friendSumElem = document.querySelector('#friendSum');
+const calculateButton = document.querySelector('#calculateButton');
+
+
+function calculateTip(sum, tip) {
+    const calculatedTip = parseInt(sum) * parseFloat(tip);
+    const total = parseInt(sum) + calculatedTip;
+
+    return total;
+}
+function divideTotal(total, numberOfFriends) {
+    const sumDivided = total / parseInt(numberOfFriends);
+    return sumDivided;
+}
+
+function showDividedSum(sum) {
+    inputFormElem.classList.toggle('hide');
+    showSumElem.classList.toggle('hide');
+    friendSumElem.innerHTML = sum + ' kr';
+}
+
+calculateButton.addEventListener('click', () => {
+    const sum = document.querySelector('#sum').value;
+    const numberOfFriends = document.querySelector('#numberOfFriends').value;
+    const tip = document.querySelector('#tip').value;
+
+    const total = calculateTip(sum, tip);
+    const friendSum = divideTotal(total, numberOfFriends);
+
+    showDividedSum(friendSum);
+});
